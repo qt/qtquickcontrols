qt/qtquickcontrols

commit 060a6afdf851ca4dbe05f19f3ad3cd486740fafa

ScrollView: Fix when the item added is not a Flickable
The Flickable item created becomes the parent of the ScrollView
contentItem.

Change-Id: I488d9f8ffc02a1de1158b279e0971473a3386e2c
Reviewed-by: Jens Bache-Wiig <jens.bache-wiig@digia.com>

src/controls/ScrollView.qml
@@ -146,7 +146,7 @@ FocusScope {
             internal.flickableItem = contentItem // "Use content if it is a flickable
         } else {
             internal.flickableItem = flickableComponent.createObject(this)
-            contentItem.parent = flickableItem.contentItem
+            contentItem.parent = internal.flickableItem
         }
         internal.flickableItem.parent = viewportItem
         internal.flickableItem.anchors.fill = viewportItem

src/private/ScrollViewHelper.qml
@@ -69,8 +69,8 @@ Item {
             recursionGuard = true
             wheelarea.availableWidth = viewport.width
             wheelarea.availableHeight = viewport.height
-            wheelarea.contentWidth = flickableItem.contentWidth
-            wheelarea.contentHeight = flickableItem.contentHeight
+            wheelarea.contentWidth = flickableItem !== null ? flickableItem.contentWidth : 0
+            wheelarea.contentHeight = flickableItem !== null ? flickableItem.contentHeight : 0
             recursionGuard = false
         }
     }

tests/auto/controls/data/tst_scrollview.qml
@@ -66,7 +66,7 @@ TestCase {
 
         scrollView.contentItem = bigItem
         scrollView.visible = true
-        verify(scrollView.flickableItem, "flickableItem should not be null")
+        verify(scrollView.flickableItem !== null, "flickableItem should not be null")
         verify(scrollView.flickableItem !== scrollView.contentItem)
         verify(scrollView.flickableItem.contentHeight === 0, "ContentHeight not set")
 

commit b611ad6b3a51a383b67d44bc310e0ef4425eeda5

Fix regression in ScrollView
The regression was introduced in
060a6afdf851ca4dbe05f19f3ad3cd486740fafa

The parent of our contentItem should really be the
flickables contentItem. Otherwise we don't scroll at all.

Change-Id: I75a9b68134bb2098f59436d23ff79ec600321963
Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@digia.com>

src/controls/ScrollView.qml
@@ -146,7 +146,7 @@ FocusScope {
             internal.flickableItem = contentItem // "Use content if it is a flickable
         } else {
             internal.flickableItem = flickableComponent.createObject(this)
-            contentItem.parent = internal.flickableItem
+            contentItem.parent = internal.flickableItem.contentItem
         }
         internal.flickableItem.parent = viewportItem
         internal.flickableItem.anchors.fill = viewportItem