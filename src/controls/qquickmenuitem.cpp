5520918af2cfb00618765820df2a28916a12c626
Andy Shaw <andy.shaw@digia.com>
2013-09-06 10:16:23 +0200

    Set the menu role for the QPlatformMenuItem to the default
    QAction has a default menu role which is passed on to the
    QPlatformMenuItem. But as QtQuickControls does not use QAction, we need
    to set the default menu role explicitly to TextHeuristicRole. This fixes
    the merging of menu items on Mac.
    
    Task-number: QTBUG-31883
    Change-Id: I24f0e265f8ceca376db9970d62e8f79a646fef3b
    Reviewed-by: Jens Bache-Wiig <jens.bache-wiig@digia.com>
    Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@digia.com>

--- src/controls/qquickmenuitem.cpp
+++ src/controls/qquickmenuitem.cpp
@@ -55,6 +55,8 @@ QQuickMenuBase::QQuickMenuBase(QObject *parent)
     : QObject(parent), m_visible(true), m_parentMenu(0), m_container(0), m_visualItem(0)
 {
     m_platformItem = QGuiApplicationPrivate::platformTheme()->createPlatformMenuItem();
+    if (m_platformItem)
+        m_platformItem->setRole(QPlatformMenuItem::TextHeuristicRole);
 }
 
 QQuickMenuBase::~QQuickMenuBase()
