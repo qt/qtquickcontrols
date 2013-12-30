55077b4f6cd6281cf020fb301fdafd3fd6e02df4
Shawn Rutledge <shawn.rutledge@digia.com>
2013-12-30 16:55:41 +0100

    QtQuick.Dialogs FileDialog: default mode is ExistingFile
    Previously did not sync the mode with the state of the selectExisting
    property on construction.
    (cherry-picked from qtdeclarative/fa5db4201945565bd2c5ff42ab233f497666003f)
    
    Task-number: QTBUG-35877
    Change-Id: Ifb630fb51c463025ec2f6a1932714f354552c234
    Reviewed-by: Liang Qi <liang.qi@digia.com>

--- src/dialogs/qquickabstractfiledialog.cpp
+++ src/dialogs/qquickabstractfiledialog.cpp
@@ -57,6 +57,7 @@ QQuickAbstractFileDialog::QQuickAbstractFileDialog(QObject *parent)
     , m_selectMultiple(false)
     , m_selectFolder(false)
 {
+    updateModes();
     connect(this, SIGNAL(accepted()), this, SIGNAL(selectionAccepted()));
 }
 
