qt/qtquickcontrols

commit 533b677351064177e1ba2d3082bb010e9f0ec473

Let ApplicationWindowStyle provide the layout
Change-Id: Ic14c153d57857efb5ad26e01f58ff3363b189b85
Reviewed-by: Jochen Seemann <seemann.jochen@gmail.com>
Reviewed-by: Jan Arve Sæther <jan-arve.saether@digia.com>
Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@digia.com>

src/controls/ApplicationWindow.qml
@@ -155,7 +155,10 @@ Window {
     property alias __style: styleLoader.item
 
     /*! \internal */
-    property real __topBottomMargins: contentArea.y + statusBarArea.height
+    property alias __panel: panelLoader.item
+
+    /*! \internal */
+    property real __topBottomMargins: contentArea.y + __panel.statusBarArea.height
     /*! \internal
         There is a similar macro QWINDOWSIZE_MAX in qwindow_p.h that is used to limit the
         range of QWindow::maximum{Width,Height}
@@ -190,11 +193,6 @@ Window {
 
     maximumWidth: Math.min(__qwindowsize_max, contentArea.maximumWidth)
     maximumHeight: Math.min(__qwindowsize_max, contentArea.maximumHeight + __topBottomMargins)
-    onToolBarChanged: { if (toolBar) { toolBar.parent = toolBarArea } }
-
-    onStatusBarChanged: { if (statusBar) { statusBar.parent = statusBarArea } }
-
-    onVisibleChanged: { if (visible && menuBar) { menuBar.__parentWindow = root } }
 
     /*! \internal */
     default property alias data: contentArea.data
@@ -205,76 +203,38 @@ Window {
     // QTBUG-35049: Windows is removing features we didn't ask for, even though Qt::CustomizeWindowHint is not set
     // Otherwise Qt.Window | Qt.WindowFullscreenButtonHint would be enough
 
-    Item {
-        id: backgroundItem
+    Loader {
+        id: panelLoader
         anchors.fill: parent
+        sourceComponent: __style ? __style.panel : null
+        onStatusChanged: if (status === Loader.Error) console.error("Failed to load Style for", root)
+        Loader {
+            id: styleLoader
+            sourceComponent: style
+            property var __control: root
+            property QtObject styleData: QtObject {
+                readonly property bool hasColor: root.color != "#ffffff"
+            }
+            onStatusChanged: if (status === Loader.Error) console.error("Failed to load Style for", root)
+        }
+
+        Binding { target: toolBar; property: "parent"; value: __panel.toolBarArea }
+        Binding { target: statusBar; property: "parent"; value: __panel.statusBarArea }
+
+        Binding {
+            property: "parent"
+            target: menuBar ? menuBar.__contentItem : null
+            when: menuBar && !menuBar.__isNative
+            value: __panel.menuBarArea
+        }
+        Binding { target: menuBar; property: "__parentWindow"; value: root }
 
         Keys.forwardTo: menuBar ? [menuBar.__contentItem] : []
 
-        Loader {
-            id: backgroundLoader
-            anchors.fill: parent
-            sourceComponent: __style ? __style.background : null
-            onStatusChanged: if (status === Loader.Error) console.error("Failed to load Style for", root)
-            Loader {
-                id: styleLoader
-                sourceComponent: style
-                property var __control: root
-                property QtObject styleData: QtObject {
-                    readonly property bool hasColor: root.color != "#ffffff"
-                }
-                onStatusChanged: if (status === Loader.Error) console.error("Failed to load Style for", root)
-            }
-        }
-
         ContentItem {
             id: contentArea
-            anchors.top: toolBarArea.bottom
-            anchors.left: parent.left
-            anchors.right: parent.right
-            anchors.bottom: statusBarArea.top
-        }
-
-        Item {
-            id: toolBarArea
-            anchors.top: parent.top
-            anchors.left: parent.left
-            anchors.right: parent.right
-            implicitHeight: childrenRect.height
-            height: visibleChildren.length > 0 ? implicitHeight: 0
-        }
-
-        Item {
-            id: statusBarArea
-            anchors.bottom: parent.bottom
-            anchors.left: parent.left
-            anchors.right: parent.right
-            implicitHeight: childrenRect.height
-            height: visibleChildren.length > 0 ? implicitHeight: 0
-        }
-
-        onVisibleChanged: if (visible && menuBar) menuBar.__parentWindow = root
-
-        states: State {
-            name: "hasMenuBar"
-            when: menuBar && !menuBar.__isNative
-
-            ParentChange {
-                target: menuBar.__contentItem
-                parent: backgroundItem
-            }
-
-            PropertyChanges {
-                target: menuBar.__contentItem
-                x: 0
-                y: 0
-                width: backgroundItem.width
-            }
-
-            AnchorChanges {
-                target: toolBarArea
-                anchors.top: menuBar.__contentItem.bottom
-            }
+            anchors.fill: parent
+            parent: __panel.contentArea
         }
     }
 }

src/controls/MenuBar.qml
@@ -90,6 +90,7 @@ MenuBarPrivate {
         active: !root.__isNative
         focus: true
         Keys.forwardTo: [item]
+        width: parent && active ? parent.width : 0
         property bool altPressed: item ? item.__altPressed : false
     }
 

src/controls/Styles/Base/ApplicationWindowStyle.qml
@@ -85,4 +85,52 @@ QtObject {
         visible: !styleData.hasColor
         color: SystemPaletteSingleton.window(true)
     }
+
+    /*! \internal */
+    property Component panel: Item {
+        readonly property alias contentArea: contentArea
+        readonly property alias menuBarArea: menuBarArea
+        readonly property alias toolBarArea: toolBarArea
+        readonly property alias statusBarArea: statusBarArea
+
+        Loader {
+            anchors.fill: parent
+            sourceComponent: background
+        }
+
+        Item {
+            id: contentArea
+            anchors.top: toolBarArea.bottom
+            anchors.left: parent.left
+            anchors.right: parent.right
+            anchors.bottom: statusBarArea.top
+        }
+
+        Item {
+            id: toolBarArea
+            anchors.top: parent.menuBarArea.bottom
+            anchors.left: parent.left
+            anchors.right: parent.right
+            implicitHeight: childrenRect.height
+            height: visibleChildren.length > 0 ? implicitHeight: 0
+        }
+
+        Item {
+            id: menuBarArea
+            anchors.top: parent.top
+            anchors.left: parent.left
+            anchors.right: parent.right
+            implicitHeight: childrenRect.height
+            height: visibleChildren.length > 0 ? implicitHeight: 0
+        }
+
+        Item {
+            id: statusBarArea
+            anchors.bottom: parent.bottom
+            anchors.left: parent.left
+            anchors.right: parent.right
+            implicitHeight: childrenRect.height
+            height: visibleChildren.length > 0 ? implicitHeight: 0
+        }
+    }
 }

commit bbcfed6cac95a7d5bea9f29974150e5505e4c612

ApplicationWindow: fix top/bottom margins
533b677 broke __topBottomMargins. The y-coordinate of contentArea is
now always 0. It must use the y-coordinate of the corresponding style
layout item instead.

Change-Id: I5316d319f0f12553ecc12a04561c0235100f4dd5
Reviewed-by: Jan Arve Sæther <jan-arve.saether@digia.com>

src/controls/ApplicationWindow.qml
@@ -158,7 +158,7 @@ Window {
     property alias __panel: panelLoader.item
 
     /*! \internal */
-    property real __topBottomMargins: contentArea.y + __panel.statusBarArea.height
+    property real __topBottomMargins: __panel.contentArea.y + __panel.statusBarArea.height
     /*! \internal
         There is a similar macro QWINDOWSIZE_MAX in qwindow_p.h that is used to limit the
         range of QWindow::maximum{Width,Height}