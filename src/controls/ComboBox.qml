richardmg/qtcomponents

Add EditMenu to input controls
Add EditMenu to TextArea, TextField, ComboBox and
SpinBox.

Change-Id: Ic2fed55c467dde65334006d252f4052430f3f40c

src/controls/ComboBox.qml
@@ -222,6 +222,15 @@ Control {
     property alias validator: input.validator
 
     /*!
+        \qmlproperty Menu TextArea::menu
+        \since QtQuick.Controls 1.3
+
+        This property contains the edit menu for working
+        with text selection.
+    */
+    property Menu menu: input.editMenu.defaultMenu
+
+    /*!
         \qmlproperty bool ComboBox::acceptableInput
         \since QtQuick.Controls 1.1
 

src/controls/TextField.qml
@@ -434,6 +434,15 @@ Control {
     property alias validator: textInput.validator
 
     /*!
+        \qmlproperty Menu TextArea::menu
+        \since QtQuick.Controls 1.3
+
+        This property contains the edit menu for working
+        with text selection.
+    */
+    property Menu menu: textInput.editMenu.defaultMenu
+
+    /*!
         \qmlsignal TextField::accepted()
 
         This signal is emitted when the Return or Enter key is pressed.

qt/qtquickcontrols

Reviewed-by: J-P Nurmi <jpnurmi@digia.com>

src/controls/ComboBox.qml
@@ -222,6 +222,18 @@ Control {
     property alias validator: input.validator
 
     /*!
+        \qmlproperty Menu ComboBox::menu
+        \since QtQuick.Controls 1.3
+
+        This property contains the edit menu for working
+        with text selection. Set it to \c null if no menu
+        is wanted.
+
+        \note The menu is only in use when \l editable is \c true
+    */
+    property Menu menu: input.editMenu.defaultMenu
+
+    /*!
         \qmlproperty bool ComboBox::acceptableInput
         \since QtQuick.Controls 1.1
 

src/controls/TextField.qml
@@ -434,6 +434,16 @@ Control {
     property alias validator: textInput.validator
 
     /*!
+        \qmlproperty Menu TextField::menu
+        \since QtQuick.Controls 1.3
+
+        This property contains the edit menu for working
+        with text selection. Set it to \c null if no menu
+        is wanted.
+    */
+    property Menu menu: textInput.editMenu.defaultMenu
+
+    /*!
         \qmlsignal TextField::accepted()
 
         This signal is emitted when the Return or Enter key is pressed.