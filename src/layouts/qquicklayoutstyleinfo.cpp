89d67ca33fcd1dd19278d248007d8aef4097407b
J-P Nurmi <jpnurmi@theqtcompany.com>
2014-10-30 15:29:32 +0100

    Layouts: increase the default spacing on mobile platforms
    Change-Id: I7aff0236a08465c05559bd8a2e8a60b556fbc4ba
    Reviewed-by: Mitch Curtis <mitch.curtis@digia.com>
    Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@theqtcompany.com>

--- src/layouts/qquicklayoutstyleinfo.cpp
+++ src/layouts/qquicklayoutstyleinfo.cpp
@@ -44,11 +44,18 @@ QQuickLayoutStyleInfo::QQuickLayoutStyleInfo()
 
 qreal QQuickLayoutStyleInfo::spacing(Qt::Orientation /*orientation*/) const
 {
+#if defined(Q_OS_ANDROID) || defined(Q_OS_IOS) || defined(Q_OS_BLACKBERRY) || defined(Q_OS_QNX) || defined(Q_OS_WINRT)
+    // On Android and iOS the default spacing between each UI element is 8dp
+    qreal spacing = 8.0;
+#else
     qreal spacing = 5.0;
-#ifndef Q_OS_MAC
-    // On mac the DPI is always 72 so we should not scale it
+#endif
+
+#ifndef Q_OS_OSX
+    // On OS X the DPI is always 72 so we should not scale it
     spacing = qRound(spacing * (qreal(qt_defaultDpiX()) / 96.0));
 #endif
+
     return spacing;
 }
 
