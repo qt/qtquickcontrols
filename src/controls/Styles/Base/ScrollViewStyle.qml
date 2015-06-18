qt/qtquickcontrols

commit f7b3f11b47768f47f625874c1bd45da4296c0f82

Default to non-transient scroll bars in non-mobile environments
A similar "fix" has been introduced for the scroll bar interactivity;
also a platform style is free to override this property.

Change-Id: I097b565ff27a3fbcea07134a9b358e066a51e980
Task-number: QTBUG-41591
Reviewed-by: David Edmundson <davidedmundson@kde.org>
Reviewed-by: J-P Nurmi <jpnurmi@theqtcompany.com>

src/controls/Styles/Base/ScrollViewStyle.qml
@@ -65,7 +65,7 @@ Style {
         appear when the content is scrolled and disappear when they are no longer needed.
 
         The default value is platform dependent. */
-    property bool transientScrollBars: Settings.hasTouchScreen
+    property bool transientScrollBars: !Settings.isMobile && Settings.hasTouchScreen
 
     /*! This Component paints the frame around scroll bars. */
     property Component frame: Rectangle {

commit 234dc90be6850135b3b54c98fabe71eff1c6071c

Fix transientScrollBars condition
It should only ever be enabled on mobile.
Fixes a regression introduced by f7b3f11b47768f47f625874c1bd45da4296c0f82

Change-Id: I0c95e6caff8e5bb745fd0d03567b0041f012c9b5
Task-number: QTBUG-41591
Reviewed-by: J-P Nurmi <jpnurmi@theqtcompany.com>

src/controls/Styles/Base/ScrollViewStyle.qml
@@ -65,7 +65,7 @@ Style {
         appear when the content is scrolled and disappear when they are no longer needed.
 
         The default value is platform dependent. */
-    property bool transientScrollBars: !Settings.isMobile && Settings.hasTouchScreen
+    property bool transientScrollBars: Settings.isMobile && Settings.hasTouchScreen
 
     /*! This Component paints the frame around scroll bars. */
     property Component frame: Rectangle {