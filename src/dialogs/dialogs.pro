qt/qtquickcontrols

commit 751f08cf46f505980b8ebe9a228a6c229b470152

Enable the use of QML caching at build time
Task-number: QTBUG-58571
Change-Id: I11c9dfb4e0fad93b2961db4213f934eca7d214df
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>
Reviewed-by: J-P Nurmi <jpnurmi@qt.io>

src/dialogs/dialogs.pro
@@ -7,7 +7,11 @@ IMPORT_VERSION = 1.2
 
 QMAKE_DOCS = $$PWD/doc/qtquickdialogs.qdocconf
 
-qtquickcompiler: DEFINES += ALWAYS_LOAD_FROM_RESOURCES
+qtquickcompiler {
+    DEFINES += ALWAYS_LOAD_FROM_RESOURCES
+} else {
+    !static: CONFIG += qmlcache
+}
 
 SOURCES += \
     qquickabstractmessagedialog.cpp \
@@ -80,7 +84,7 @@ ios|android|blackberry|winrt {
 
 QT += quick-private gui gui-private core core-private qml qml-private
 
-!static {
+!qmlcache {
     # Create the resource file
     GENERATED_RESOURCE_FILE = $$OUT_PWD/dialogs.qrc
 

src/controls/controls.pro
@@ -40,8 +40,12 @@ CONTROLS_QML_FILES = \
     ToolBar.qml \
     ToolButton.qml
 
-!qtquickcompiler: QML_FILES += $$CONTROLS_QML_FILES
-qtquickcompiler: DEFINES += ALWAYS_LOAD_FROM_RESOURCES
+qtquickcompiler {
+    DEFINES += ALWAYS_LOAD_FROM_RESOURCES
+} else {
+    QML_FILES += $$CONTROLS_QML_FILES
+    !static: CONFIG += qmlcache
+}
 
 SOURCES += $$PWD/plugin.cpp
 HEADERS += $$PWD/plugin.h
@@ -55,7 +59,7 @@ include(Shaders/shaders.pri)
 
 osx: LIBS_PRIVATE += -framework Carbon
 
-!static {
+!qmlcache {
     # Create the resource file
     GENERATED_RESOURCE_FILE = $$OUT_PWD/controls.qrc
 

src/extras/extras.pro
@@ -28,6 +28,8 @@ SOURCES += plugin.cpp \
 include(Private/private.pri)
 include(designer/designer.pri)
 
+!static: CONFIG += qmlcache
+
 OTHER_FILES += doc/src/*
 
 TR_EXCLUDE += designer/*

commit dacb91916b202208681471ba764504c7c7d086f0

Fix inclusion of QML files of the Dialogs module in static builds
This is a regression introduce by commit
751f08cf46f505980b8ebe9a228a6c229b470152. For a build with QML
Ahead-of-Time cache creation, we need the qml files in QML_FILES and
qml_module.prf takes care of installing them correctly into the file
system (caches and sources). For a static build we also need the sources
in QML_FILES, in order for their automatic inclusion in the resource
system (also by qml_module.prf).

Task-number: QTBUG-60861
Change-Id: Id9125530d8b385101994600c1224dbcb2a5333f9
Reviewed-by: Andy Shaw <andy.shaw@qt.io>

src/dialogs/dialogs.pro
@@ -84,32 +84,6 @@ ios|android|blackberry|winrt {
 
 QT += quick-private gui gui-private core core-private qml qml-private
 
-!qmlcache {
-    # Create the resource file
-    GENERATED_RESOURCE_FILE = $$OUT_PWD/dialogs.qrc
-
-    RESOURCE_CONTENT = \
-        "<RCC>" \
-        "<qresource prefix=\"/QtQuick/Dialogs\">"
-
-    for(resourcefile, DIALOGS_QML_FILES) {
-        resourcefileabsolutepath = $$absolute_path($$resourcefile)
-        relativepath_in = $$relative_path($$resourcefileabsolutepath, $$_PRO_FILE_PWD_)
-        relativepath_out = $$relative_path($$resourcefileabsolutepath, $$OUT_PWD)
-        RESOURCE_CONTENT += "<file alias=\"$$relativepath_in\">$$relativepath_out</file>"
-    }
-
-    RESOURCE_CONTENT += \
-        "</qresource>" \
-        "</RCC>"
-
-    write_file($$GENERATED_RESOURCE_FILE, RESOURCE_CONTENT)|error("Aborting.")
-
-    RESOURCES += $$GENERATED_RESOURCE_FILE
-    # In case of a debug build, deploy the QML files too
-    !qtquickcompiler:CONFIG(debug, debug|release): QML_FILES += $$DIALOGS_QML_FILES
-} else {
-    QML_FILES += $$DIALOGS_QML_FILES
-}
+QML_FILES += $$DIALOGS_QML_FILES
 
 load(qml_plugin)