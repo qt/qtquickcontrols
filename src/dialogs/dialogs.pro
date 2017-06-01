qt/qtquickcontrols

commit dacb91916b202208681471ba764504c7c7d086f0

Fix inclusion of QML files of the Dialogs module in static builds
This is a regression introduce by commit
751f08cf46f505980b8ebe9a228a6c229b470152. For a build with QML
Ahead-of-Time cache creation, we need the qml files in QML_FILES and
qml_module.prf takes care of installing them correctly into the file
system (caches and sources). For a static build we also need the sources
in QML_FILES, in order for their automatic inclusion in the resource
system (also by qml_module.prf).

Task-number: QTBUG-60861
Change-Id: Id9125530d8b385101994600c1224dbcb2a5333f9
Reviewed-by: Andy Shaw <andy.shaw@qt.io>

src/dialogs/dialogs.pro
@@ -84,32 +84,6 @@ ios|android|blackberry|winrt {
 
 QT += quick-private gui gui-private core core-private qml qml-private
 
-!qmlcache {
-    # Create the resource file
-    GENERATED_RESOURCE_FILE = $$OUT_PWD/dialogs.qrc
-
-    RESOURCE_CONTENT = \
-        "<RCC>" \
-        "<qresource prefix=\"/QtQuick/Dialogs\">"
-
-    for(resourcefile, DIALOGS_QML_FILES) {
-        resourcefileabsolutepath = $$absolute_path($$resourcefile)
-        relativepath_in = $$relative_path($$resourcefileabsolutepath, $$_PRO_FILE_PWD_)
-        relativepath_out = $$relative_path($$resourcefileabsolutepath, $$OUT_PWD)
-        RESOURCE_CONTENT += "<file alias=\"$$relativepath_in\">$$relativepath_out</file>"
-    }
-
-    RESOURCE_CONTENT += \
-        "</qresource>" \
-        "</RCC>"
-
-    write_file($$GENERATED_RESOURCE_FILE, RESOURCE_CONTENT)|error("Aborting.")
-
-    RESOURCES += $$GENERATED_RESOURCE_FILE
-    # In case of a debug build, deploy the QML files too
-    !qtquickcompiler:CONFIG(debug, debug|release): QML_FILES += $$DIALOGS_QML_FILES
-} else {
-    QML_FILES += $$DIALOGS_QML_FILES
-}
+QML_FILES += $$DIALOGS_QML_FILES
 
 load(qml_plugin)

commit ae688459bc845ab919ce256f315843d97ba9ff32

Fix support for using QtQuick Dialogs with the QML compiler
This is a regression from commit
dacb91916b202208681471ba764504c7c7d086f0. Make sure to include the
dialogs sources in the resource system when using the compiler.

Task-number: QTBUG-60991
Change-Id: I35c3e2e677e4f44ec56fe02b086ce08a8b765d37
Reviewed-by: J-P Nurmi <jpnurmi@qt.io>

src/dialogs/dialogs.pro
@@ -7,12 +7,6 @@ IMPORT_VERSION = 1.2
 
 QMAKE_DOCS = $$PWD/doc/qtquickdialogs.qdocconf
 
-qtquickcompiler {
-    DEFINES += ALWAYS_LOAD_FROM_RESOURCES
-} else {
-    !static: CONFIG += qmlcache
-}
-
 SOURCES += \
     qquickabstractmessagedialog.cpp \
     qquickplatformmessagedialog.cpp \
@@ -85,5 +79,14 @@ ios|android|blackberry|winrt {
 QT += quick-private gui gui-private core core-private qml qml-private
 
 QML_FILES += $$DIALOGS_QML_FILES
+
+qtquickcompiler {
+    DEFINES += ALWAYS_LOAD_FROM_RESOURCES
+    dialogs.files = $$QML_FILES
+    dialogs.prefix = /QtQuick/Dialogs
+    RESOURCES += dialogs
+} else {
+    !static: CONFIG += qmlcache
+}
 
 load(qml_plugin)